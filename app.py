216830498831f4d4b37abcde3ad14d9ded001fbd
Eduardo Henrique Lisboa Alves <108145431+Eduardo-Lisboa@users.noreply.github.com>
2024-06-27 03:02:58 +0000

    configurando o side_bar

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
-import streamlit as at
+import streamlit as st# type: ignore
+import pandas as pd
+import numpy as np
+import yfinance as yf # type: ignore
+import plotly.express as px
+from datetime import datetime
+from streamlit_extras.metric_cards import style_metric_cards # type: ignore
+from streamlit_extras.grid import grid# type: ignore
 
-at.title("Home broaker")
+st.title("Home broaker")
+
+def build_sidebar():
+    st.image("image/b3.png")
+
+def build_main():
+    pass
+
+
+with st.sidebar:
+    build_sidebar()
